5694fa439611503f02d850a5603fc823179c6b45
Héctor Fiorentino <hfiorentino@ifdevelopers.com.ar>
2014-06-06 14:41:22 -0300

    Cambio de APP ID facebook

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function main(){
-    //$.mobile.changePage($("#pageexamenes"));
+    //$.mobile.changePage($("#pageregistr"));
     if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
             if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
             if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
@@ -21,7 +21,7 @@ function main(){
             FB.Event.subscribe('auth.statusChange', function(response) {
                                //alert('auth.statusChange event');
                                });
-    FB.init({ appId: "1537747696452721", nativeInterface: CDV.FB, useCachedDialogs: false });
+    FB.init({ appId: "1536370399923784", nativeInterface: CDV.FB, useCachedDialogs: false });
 
     var update;
     if(window.localStorage.getItem("lastUpdate")){
